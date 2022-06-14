fb2822ab8f75c5961bc0d29254dc50ba21ccf5af
Bhupendra <bhupendrasimha@gmail.com>
2022-06-14 17:34:37 +0530

    firebase integrated

--- config/admin.js
+++ config/admin.js
@@ -2,11 +2,11 @@
 //FIREBASE ADMIN SDK INTEGRATION
 
 const admin =require("firebase-admin")
-const serviceAccout= require("../hiccup-398c2-firebase-adminsdk-ivmnv-b3590cac2f.json")
+const serviceAccout= require("../hiccup-admin.json")
 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccout),
-  databaseURL: 'https://hiccup-398c2.firebaseio.com'
+  databaseURL:process.env.firebaseURL 
 });
 module.exports= admin
